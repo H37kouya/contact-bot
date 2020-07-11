87774f084d689727e977c75d07d70928d143f11f
H37kouya <h37kouya@gmail.com>
2020-07-11 14:42:15 +0900

    test CI用の修正

--- src/config/dotenv.go
+++ src/config/dotenv.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
@@ -12,6 +13,10 @@ func init() {
 
 // loadDotEnv .envファイルをロードする
 func loadDotEnv() {
+	if os.Getenv("CI") == "true" {
+		return
+	}
+
 	if godotenv.Load(".env") != nil {
 		log.Fatal("Error loading .env file")
 	}
